750922da00d232e0b7206cad621c2cc22360ead7
bright <liyuliang001@gmail.com>
2020-08-05 16:06:01 -0400

    add gen_traffic_files.sh

---
+++ gen_traffic_files.sh
@@ -0,0 +1,2 @@
+python traffic_gen/traffic_gen.py -c traffic_gen/WebSearch_distribution.txt -n 320 -l 0.5 -b 100G -t 0.2 > simulation/mix/wb50_b100.txt
+python traffic_gen/traffic_gen.py -c traffic_gen/FbHdp_distribution.txt -n 320 -l 0.5 -b 100G -t 0.2 > simulation/mix/fb50_b100.txt
